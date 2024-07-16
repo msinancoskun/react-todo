b603fa65d7971c2bb11df43ab31dc172b44bb987
msinancoskun <sinancoskun2345@gmail.com>
2024-07-16 23:32:12 +0300

    local storage impl

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, ChangeEvent, useState } from "react";
+import React, { FC, ChangeEvent, useState, useEffect } from "react";
 import { ITask } from "./Interfaces";
 import "./App.css";
 import TodoTask from "./Components/TodoTask";
@@ -7,22 +7,48 @@ const App: FC = () => {
   const [task, setTask] = useState<string>("");
   const [deadline, setDeadline] = useState<number>(0);
   const [todoList, setTodoList] = useState<ITask[]>([]);
+  const [errorMessage, setErrorMessage] = useState<string>("");
+
+  useEffect(() => {
+    if (todoList.length === 0) {
+      const storage = localStorage!.getItem("todoList");
+
+      if (storage) {
+        const data = JSON.parse(storage) as ITask[];
+        setTodoList([...todoList, ...data]);
+      }
+    } else {
+      localStorage.setItem("todoList", JSON.stringify(todoList));
+    }
+  }, [todoList]);
 
   const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
     if (event.target.name === "task") setTask(event.target.value);
     else if (event.target.name === "deadline")
-      setDeadline(Number(event.target.value));
+      if (Number(event.target.value) > 0)
+        setDeadline(Number(event.target.value));
+      else
+        console.log(
+          `Deadline value must be greater than ${event.target.value}`
+        );
   };
 
   const addTask = (): void => {
-    const newTask = {
-      name: task,
-      deadline: deadline,
-    };
+    if (task && deadline > 0) {
+      const newTask = {
+        name: task,
+        deadline: deadline,
+      };
 
-    setTodoList([...todoList, newTask]);
-    setTask("");
-    setDeadline(0);
+      setTodoList([...todoList, newTask]);
+      setTask("");
+      setDeadline(0);
+      setErrorMessage("");
+    } else {
+      setErrorMessage(
+        "Task cannot be empty and deadline must be greater than zero!"
+      );
+    }
   };
 
   const completeTask = (taskNameToDelete: string): void => {
@@ -31,11 +57,14 @@ const App: FC = () => {
         return task.name !== taskNameToDelete;
       })
     );
+
+    localStorage.removeItem("todoList");
   };
 
   return (
     <div className="App">
-      <div className="header">
+      <div className="header" style={{ display: "flex" }}>
+        {errorMessage && <div className="error"> {errorMessage} </div>}
         <div className="inputContainer">
           <input
             type="text"
